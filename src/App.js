62e75d00eecac2a6a01d6d3a4c4897b99dc3c46b
Daniel S <iam@w-b.dev>
2020-11-06 00:56:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    function authenticate() {
+        return gapi.auth2.getAuthInstance()
+            .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"}).then(_ => setIsAuthenticated(true));
+    }
+
+    function loadClient() {
+        gapi.client.setApiKey(process.env.REACT_APP_API_KEY);
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest");
+    }
+
+    function execute() {
+        return gapi.client.youtube.subscriptions.list({
+            "part": [
+                "snippet"
+            ],
+            "mine": true
+        }).then(setState);
+    }
+
+    function loadApi() {
+        const script = document.createElement("script");
+        script.src = "https://apis.google.com/js/api.js";
+        document.body.appendChild(script);
+        script.onload = () => {
+            gapi.load('client:auth2', function () {
+                gapi.auth2.init({client_id: process.env.REACT_APP_CLIENT_ID});
+            });
+        }
+    }
+
+    const handleAuth = () => {
+        authenticate().then(loadClient);
+    }
+
+    useEffect(() => {
+        loadApi();
+    }, []);
+    useEffect(() => {
+        console.info(state ? state.result.items : 'nothing yet');
+    }, [state]);
+    return (
+        <div className="App">
+            <h1>Youtube app</h1>
+            {state && state.result.items.map(e => <article key={e.id}>{e.id}</article>)}
+            <button disabled={!isAuthenticated ? false : true} onClick={handleAuth}>auth</button>
+            <button disabled={isAuthenticated ? false : true} onClick={execute}>execute</button>
+        </div>
+    );
 }
 
 export default App;
