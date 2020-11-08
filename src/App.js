4dbcb8f116403b8c415a45321eb457f45afc3430
Daniel S <iam@w-b.dev>
2020-11-08 15:50:34 -0500

    loads YT subs. no action. several TODO.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-undef */
 import React, {useEffect, useState} from "react";
+import Loader from "./components/Loader";
+import './App.css'
 
 function App() {
     const [state, setState] = useState(null);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    function authenticate() {
-        return gapi.auth2.getAuthInstance()
-            .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"}).then(_ => setIsAuthenticated(true));
-    }
+    const [isLoading, setIsLoading] = useState(false);
 
     function loadClient() {
         gapi.client.setApiKey(process.env.REACT_APP_API_KEY);
         return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest");
+    }
+
+    function authenticate() {
+        return gapi.auth2.getAuthInstance()
+            .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"});
     }
 
     function execute() {
@@ -19,7 +23,8 @@ function App() {
             "part": [
                 "snippet"
             ],
-            "mine": true
+            "mine": true,
+            "maxResults": 50,
         }).then(setState);
     }
 
@@ -35,23 +40,94 @@ function App() {
     }
 
     const handleAuth = () => {
-        authenticate().then(loadClient);
+        setIsLoading(true);
+        authenticate()
+            .then(loadClient)
+            .then(_ => {
+                setIsAuthenticated(true);
+                execute();
+                setIsLoading(false);
+            });
     }
 
     useEffect(() => {
         loadApi();
     }, []);
     useEffect(() => {
-        console.info(state ? state.result.items : 'nothing yet');
+        console.info('Current state:', state ? state.result.items : state);
     }, [state]);
     return (
         <div className="App">
-            <h1>Youtube app</h1>
-            {state && state.result.items.map(e => <article key={e.id}>{e.id}</article>)}
-            <button disabled={!isAuthenticated ? false : true} onClick={handleAuth}>auth</button>
-            <button disabled={isAuthenticated ? false : true} onClick={execute}>execute</button>
+            <header>
+                <div className="app-title-wrapper">
+                    <h1 className="app-title">YT sub manager</h1>
+                    <h2 className="app-user">load user email here -- also date data was last updated</h2>
+                </div>
+                <ul className="header-todo-list">
+                    <li>Images should have skeleton placeholders that are dismissed on load.</li>
+                    <li>Save user actions in Firebase (after successful completion of action).</li>
+                    <li>On initial load, fetch user records (video list, favorites, etc) from Firebase. If more than 1
+                        day old, ask Youtube instead
+                    </li>
+                    <li>Allow *undo* action.</li>
+                    <li>Optimistic UI (deletion, archival, favorite).</li>
+                    <li>Implement Pagination (response has nextPageToken).</li>
+                    <li>BUG: horizontal scrolling.</li>
+                    <li>BUG: go to bottom button not working.</li>
+                </ul>
+            </header>
+            <main>
+                {
+                    isLoading && <Loader/>
+                }
+                {state && (
+                    <section className="main--channels-wrapper">
+                        {
+                            state.result.items.map(e => <ChannelEntry key={e.id} entry={e}/>)
+                        }
+                    </section>
+                )}
+                {!isLoading && !isAuthenticated && (
+                    <button disabled={isAuthenticated ? true : false} onClick={handleAuth}>auth</button>)}
+                    <div id="hidden-target">...</div>
+            </main>
+            <footer>
+                <button onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>Back to top</button>
+                {/*<button onClick={() => window.scrollTo({bottom: 0, left: 0, behavior: 'smooth'})}>Jump to bottom</button>*/}
+                <a href="#hidden-target">Bottom target</a>
+            </footer>
         </div>
     );
+}
+
+const ChannelEntry = ({entry}) => {
+    return (
+        <article className="card-root">
+            <img className="card-image" height="88px" width="88px" src={entry.snippet.thumbnails.default.url}
+                 alt={entry.snippet.title}/>
+            <div className="channel-description-wrapper">
+                <b className="card-title">{entry.snippet.title}</b>
+                <small className="channel-id" title={entry.id}>{entry.id}</small>
+            </div>
+            <div className="controls-row">
+                <button onFocus={(e) => {
+                    e.target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'center',
+                        inline: 'center'
+                    });
+                }}>Delete</button>
+                <select name="other-channels" id="other-channels" disabled={true}>
+                    <option value="" defaultValue={true} disabled={true} hidden={true}>Move to another channel</option>
+                    <option value="channel-b">Channel B</option>
+                    <option value="channel-c">Channel C</option>
+                    <option value="channel-d">Channel D</option>
+                </select>
+                <button disabled={true}>Favorite</button>
+                <button disabled={true} title="(don't show again in this app)">Archive</button>
+            </div>
+        </article>
+    )
 }
 
 export default App;
